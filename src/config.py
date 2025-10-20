1effe391254be76d1550e16465d7113ca4b4e39c
github-main-user <kolya.skripn@mail.ru>
2025-10-20 10:50:49 +0300

    feat: add and use new env settings

--- src/config.py
+++ src/config.py
@@ -7,6 +7,13 @@ class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str
     TELEGRAM_CHAT_ID: str
 
+    FEE_PERCENT: float
+    DAYS_TO_MATURITY_MAX: int
+    ANNUAL_YIELD_MIN: float
+    ANNUAL_YIELD_MAX: float
+    BOND_SUM_MAX: float
+    BOND_SUM_MAX_SINGLE: float
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
--- src/logic.py
+++ src/logic.py
@@ -1,5 +1,6 @@
 import logging
 
+from .config import settings
 from .market.services import fetch_bonds, update_market_data
 from .market.utils import filter_bonds
 from .telegram.services import send_telegram_message
@@ -11,18 +12,23 @@ def logic() -> None:
     bonds = fetch_bonds()
     logger.info("Got %s bonds", len(bonds))
 
-    bonds = filter_bonds(bonds, maximum_days=30)
+    bonds = filter_bonds(bonds, maximum_days=settings.DAYS_TO_MATURITY_MAX)
     logger.info("%s bonds left after filtration", len(bonds))
 
     for bond in bonds:
         try:
-            update_market_data(fee_percent=0.05, bond=bond)
+            update_market_data(fee_percent=settings.FEE_PERCENT, bond=bond)
         except Exception as e:
             logger.error(f"Got an exception: %s", e)
 
-    for bond in bonds:
-        if bond.market_data and bond.market_data.annual_yield >= 20:
-            message = (
-                f"Bond {bond.ticker} has annual yeild {bond.market_data.annual_yield}%"
+        if not bond.market_data:
+            return
+
+        if (
+            settings.ANNUAL_YIELD_MIN
+            <= bond.market_data.annual_yield
+            <= settings.ANNUAL_YIELD_MAX
+        ):
+            send_telegram_message(
+                f"Bond `{bond.ticker}` has annual yeild {bond.market_data.annual_yield}%"
             )
-            send_telegram_message(message)
