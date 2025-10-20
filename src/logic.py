b94af850b770521a539e1093f683b8237aa8ff33
github-main-user <kolya.skripn@mail.ru>
2025-10-20 20:34:57 +0300

    feat: calculate how much bonds are available to buy before buying

--- src/logic.py
+++ src/logic.py
@@ -6,9 +6,10 @@ from .market.services import (
     fetch_bonds,
     get_account_balance,
     get_account_id,
+    get_existing_bonds,
     update_market_data,
 )
-from .market.utils import filter_bonds
+from .market.utils import filter_bonds, normalize_quotation
 from .telegram.services import send_telegram_message
 
 logger = logging.getLogger(__name__)
@@ -22,14 +23,13 @@ def logic() -> None:
     logger.info("%s bonds left after filtration", len(bonds))
 
     max_annual_yield = 0
-
     for bond in bonds:
         logger.info("Processing bond: %s", bond.ticker)
 
         try:
             update_market_data(fee_percent=settings.FEE_PERCENT, bond=bond)
         except Exception as e:
-            logger.error("Got an exception: %s", e)
+            logger.error("Got an exception during market data update: %s", e)
 
         if not bond.market_data:
             logger.info("Skipped bond: %s - No market data", bond.ticker)
@@ -43,27 +43,68 @@ def logic() -> None:
             <= settings.ANNUAL_YIELD_MAX
         ):
             logger.info(
-                f"Trying to buy bond: `%s` (%s%%) (%s bonds for current: %s₽ + real: %s₽)",
+                f"Trying to buy bond: `%s` (%s%%) (%s bonds for current: %s₽, real: %s₽)",
                 bond.ticker,
                 format(bond.market_data.annual_yield, ".2f"),
                 bond.market_data.ask_quantity,
                 format(bond.market_data.current_price, ".2f"),
                 format(bond.market_data.real_price, ".2f"),
             )
+            if bond.market_data.real_price <= 0:
+                logger.info(
+                    "Skipped bond: %s - real_price is not positive.", bond.ticker
+                )
+                continue
+
             account_id = get_account_id()
             balance = get_account_balance(account_id)
-            try:
-                if balance >= bond.market_data.real_price:
-                    buy_price = buy_bond(account_id, bond, 1)
+            existing_bond = get_existing_bonds(account_id).get(bond.ticker)
 
-                    message = f"Bought 1 {bond.ticker} for {buy_price + bond.market_data.fee:.2f}₽"
-                    logger.info(message)
+            quantity_to_buy_single = int(
+                settings.BOND_SUM_MAX_SINGLE // bond.market_data.real_price
+            )
+
+            if existing_bond:
+                current_value = normalize_quotation(
+                    existing_bond.quantity
+                ) * normalize_quotation(existing_bond.current_price)
+                allowed_budget = settings.BOND_SUM_MAX - current_value
+            else:
+                allowed_budget = settings.BOND_SUM_MAX
+
+            quantity_allowed_to_buy = 0
+            if allowed_budget > 0:
+                quantity_allowed_to_buy = int(
+                    allowed_budget // bond.market_data.real_price
+                )
+
+            quantity_available_to_buy = int(balance // bond.market_data.real_price)
+
+            quantity_to_buy = min(
+                quantity_to_buy_single,
+                quantity_allowed_to_buy,
+                quantity_available_to_buy,
+                bond.market_data.ask_quantity,
+            )
+
+            if quantity_to_buy > 0:
+                try:
+                    buy_price = buy_bond(account_id, bond, quantity_to_buy)
+                except Exception as e:
+                    logger.error("Error occurred during order post %s", e)
                 else:
-                    message = f"Cannot buy {bond.ticker} - Not enought balance"
-                    logger.warning(message)
-
-                send_telegram_message(message)
-            except Exception as e:
-                logger.error("Error ocured during order post %s", e)
+                    real_buy_price = buy_price + bond.market_data.fee
+                    message = (
+                        f"Bought {quantity_to_buy} of {bond.ticker} "
+                        f"(expected: {bond.market_data.real_price:.2f}₽, real: {real_buy_price:.2f}₽)"
+                    )
+                    logger.info(message)
+                    send_telegram_message(message)
+            else:
+                logger.info(
+                    "Skipped buying %s: calculated quantity is %s",
+                    bond.ticker,
+                    quantity_to_buy,
+                )
 
     logger.info("Maximum annual yield: %s%%", format(max_annual_yield, ".2f"))
--- src/market/services.py
+++ src/market/services.py
@@ -1,8 +1,7 @@
 import logging
 from datetime import datetime, timezone
-from pprint import pprint
 
-from tinkoff.invest import Client, OrderDirection, OrderType
+from tinkoff.invest import Client, OrderDirection, OrderType, PortfolioPosition
 
 from src.config import settings
 from src.market.utils import normalize_quotation
@@ -10,6 +9,12 @@ from src.market.utils import normalize_quotation
 from .schemas import MarketData, NBond
 
 logger = logging.getLogger(__name__)
+
+
+def get_existing_bonds(account_id: str) -> dict[str, PortfolioPosition]:
+    with Client(settings.TINVEST_TOKEN) as client:
+        positions = client.operations.get_portfolio(account_id=account_id).positions
+        return {p.ticker: p for p in positions if p.instrument_type == "bond"}
 
 
 def get_account_id() -> str:
