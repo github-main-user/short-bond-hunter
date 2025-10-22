a7b6f0ea084c657c1e133e17960fe34ad551572b
github-main-user <kolya.skripn@mail.ru>
2025-10-22 13:10:21 +0300

    feat: setup logging into file

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import logging
 from src.market.streaming import run_streaming_logic
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler("logs/app.log"),
+        logging.FileHandler("logs/latest.log", mode="w"),
+    ],
 )
 
 if __name__ == "__main__":
