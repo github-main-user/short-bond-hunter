148025090e84cb8d15bf159a443f54f15b5e14a1
github-main-user <kolya.skripn@mail.ru>
2026-03-06 20:57:26 +0300

    chore: print price per bond in logs and tg message

--- src/market/messages.py
+++ src/market/messages.py
@@ -8,7 +8,7 @@ def compose_purchase_notification(
         f"Bought {buy_quantity} of `{bond.ticker}` ({bond.annual_yield:.2f}%)\n"
         f"Available: {bond.ask_quantity}\n"
         f"Expected price: {bond.real_price * buy_quantity:.2f}₽\n"
-        f"Actual price: {buy_price:.2f}₽\n"
+        f"Actual price: {buy_price:.2f}₽\n{f' ({buy_price / buy_quantity}) per bond' if buy_quantity > 1 else ''}"
         f"Benefit: {bond.benefit * buy_quantity:.2f}₽ in {bond.days_to_maturity} days "
         f"({bond.benefit * buy_quantity / bond.days_to_maturity:.2f}₽ per day)"
     )
