db76ce894c20161a2d80be3612c50f27c0dcd241
github-main-user <kolya.skripn@mail.ru>
2026-05-28 12:24:40 +0300

    chore: update purchase message 2

--- src/market/messages.py
+++ src/market/messages.py
@@ -25,16 +25,15 @@ def compose_purchase_notification(
     buy_price: float,
     remaining_balance: float | None,
 ) -> str:
-    cost_pb_clean = bond.current_price + bond.aci_value
-    maturity_pb = bond.nominal + bond.coupons_sum
+    cost_without_commission = bond.current_price + bond.aci_value
+    expected_maturity = bond.nominal + bond.coupons_sum
     return (
         f"{buy_price:.2f}₽, {bond.annual_yield:.2f}%, {bond.days_to_maturity}d\n"
         f"Ticker: `{bond.ticker}`\n"
         f'Name: "{bond.name}"\n'
         f"Qty Purchased: {buy_quantity} / {bond.ask_quantity}\n"
-        f"Total: {buy_price:.2f}₽\n"
-        f"Cost: {cost_pb_clean * buy_quantity:.2f}₽ + {bond.commission * buy_quantity:.2f}₽ = {bond.real_price * buy_quantity:.2f}₽\n"
-        f"Maturity: {bond.nominal * buy_quantity:.2f}₽ + {bond.coupons_sum * buy_quantity:.2f}₽ = {maturity_pb * buy_quantity:.2f}₽\n"
+        f"Cost: {cost_without_commission * buy_quantity:.2f}₽ + {bond.commission * buy_quantity:.2f}₽ = {bond.real_price * buy_quantity:.2f}₽\n"
+        f"Maturity: {bond.nominal * buy_quantity:.2f}₽ + {bond.coupons_sum * buy_quantity:.2f}₽ = {expected_maturity * buy_quantity:.2f}₽\n"
         f"Benefit: {bond.benefit * buy_quantity:.2f}₽ in {bond.days_to_maturity} days "
         f"({bond.benefit * buy_quantity / bond.days_to_maturity:.2f}₽ per day)\n"
         f"{f'Remaining balance: {remaining_balance:.2f}₽' if remaining_balance is not None else ''}"
