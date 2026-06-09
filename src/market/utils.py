4e0ccf0000d5af812f183290d6216eecafffbc05
github-main-user <kolya.skripn@mail.ru>
2026-06-09 17:05:01 +0300

    feat: implement new orders api functions

--- src/market/utils.py
+++ src/market/utils.py
@@ -3,3 +3,9 @@ from t_tech.invest import MoneyValue, Quotation
 
 def normalize_quotation(money: MoneyValue | Quotation) -> float:
     return money.units + (money.nano / 1e9)
+
+
+def denormalize_quotation(value: float) -> Quotation:
+    units = int(value)
+    nano = int(round((value - units) * 1e9))
+    return Quotation(units=units, nano=nano)
