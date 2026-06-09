27b65ac96958462f0ada74952a1379655478fb0f
github-main-user <kolya.skripn@mail.ru>
2026-06-09 17:05:01 +0300

    feat: implement new orders api functions

--- src/market/api/__init__.py
+++ src/market/api/__init__.py
@@ -2,13 +2,21 @@ from .accounts import fetch_account_id, fetch_user_commission
 from .instruments import fetch_bond_by_figi, fetch_coupons_sum, fetch_raw_bonds
 from .market_data import fetch_tmon_etf_price_at
 from .operations import fetch_coupon_operation_for_repayment, fetch_operations
-from .orders import buy_bond
+from .orders import (
+    buy_bond,
+    cancel_bid_order,
+    fetch_active_bid_orders,
+    place_bid_order,
+    replace_bid_order,
+)
 from .portfolio import fetch_account_balance_rub, fetch_existing_bonds
 
 __all__ = [
     "buy_bond",
+    "cancel_bid_order",
     "fetch_account_balance_rub",
     "fetch_account_id",
+    "fetch_active_bid_orders",
     "fetch_bond_by_figi",
     "fetch_coupon_operation_for_repayment",
     "fetch_coupons_sum",
@@ -17,4 +25,6 @@ __all__ = [
     "fetch_raw_bonds",
     "fetch_tmon_etf_price_at",
     "fetch_user_commission",
+    "place_bid_order",
+    "replace_bid_order",
 ]
