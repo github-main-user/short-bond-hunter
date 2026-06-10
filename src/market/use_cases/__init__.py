e51638c4f3a39c5417632a958943fb27250efca0
github-main-user <kolya.skripn@mail.ru>
2026-06-10 11:07:08 +0300

    feat: implement bid waiter use_case

--- src/market/use_cases/__init__.py
+++ src/market/use_cases/__init__.py
@@ -1,4 +1,5 @@
+from .bid_waiter import process_bid_for_orderbook
 from .bond import process_bond
 from .maturity import process_maturity
 
-__all__ = ["process_bond", "process_maturity"]
+__all__ = ["process_bid_for_orderbook", "process_bond", "process_maturity"]
