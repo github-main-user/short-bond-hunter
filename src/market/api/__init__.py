f9d39973edac736d122eb854864e2fcec63d35a6
github-main-user <kolya.skripn@mail.ru>
2026-06-10 22:03:22 +0300

    feat: yield existing orderbook when start app

--- src/market/api/__init__.py
+++ src/market/api/__init__.py
@@ -1,6 +1,6 @@
 from .accounts import fetch_account_id, fetch_user_commission
 from .instruments import fetch_bond_by_figi, fetch_coupons_sum, fetch_raw_bonds
-from .market_data import fetch_tmon_etf_price_at
+from .market_data import fetch_orderbook, fetch_tmon_etf_price_at
 from .operations import fetch_operations
 from .orders import (
     buy_at_ask,
@@ -21,6 +21,7 @@ __all__ = [
     "fetch_coupons_sum",
     "fetch_bond_positions",
     "fetch_operations",
+    "fetch_orderbook",
     "fetch_raw_bonds",
     "fetch_tmon_etf_price_at",
     "fetch_user_commission",
