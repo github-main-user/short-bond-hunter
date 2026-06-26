c49943f6d016a6b52bf04c1e7ecc20673426f7af
github-main-user <kolya.skripn@mail.ru>
2026-06-26 16:32:24 +0300

    chore: update config for postgres

--- src/config.py
+++ src/config.py
@@ -11,6 +11,14 @@ class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str | None = None
     TELEGRAM_CHAT_ID: str | None = None
 
+    POSTGRES_HOST: str = "postgres"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+    LOG_LEVEL: str = "INFO"
+
     DAYS_TO_MATURITY_MAX: int
     ASK_MIN_ANNUAL_YIELD: float
     ASK_MAX_ANNUAL_YIELD: float
@@ -27,7 +35,10 @@ class Settings(BaseSettings):
 
     @property
     def DATABASE_URL(self) -> str:
-        return f"sqlite:///{BASE_DIR}/stats.db"
+        return (
+            f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
     @property
     def BOND_REFRESH_INTERVAL_SECONDS(self) -> int:
